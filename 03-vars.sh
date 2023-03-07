215a06c5b3b4d3f3bd58e160ccaabd9dec69d3a6
aravnanda2019 <aravnanda2019@gmail.com>
2023-03-07 10:06:16 +0530

    print in shell

--- 03-vars.sh
+++ 03-vars.sh
@@ -6,3 +6,7 @@ DATE=$(date)
 DATE1=$(date)
 echo "Hello, Good Morning, Today date is ${DATE}"
 echo "Hello, Good Morning, Today date is ${DATE1}"
+
+# Arthimatic Substitution
+ADD=$((2+3))
+echo "Addition of 2+3 = ${ADD}"
